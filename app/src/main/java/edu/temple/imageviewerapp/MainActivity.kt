6e3e7cd66770103e3ec2371058b9c908a8a6fb5c
rrubin1219 <jenrubin12@yahoo.com>
2021-09-29 08:25:56 -0400

    Added images

--- app/src/main/java/edu/temple/imageviewerapp/MainActivity.kt
+++ app/src/main/java/edu/temple/imageviewerapp/MainActivity.kt
@@ -7,5 +7,45 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.image_activity)
+
+
     }
 }
+
+/*
+* define listener in activity
+* pass it to adapter
+*
+* class ImageAdapter(): RecyclerView.Adapter<ImageAdapter.ImageViewHolder>(){
+*   class ImageViewHolder(_imageView: ImageView):RecyclerView.ViewHolder(_imageView){
+*      val imageView = _imageView
+*   }
+*
+*   onCreateViewHolder(parent:ViewGroup, viewType:Int:imageViewHOlder{
+*       val imageView = ImageView(parent.context)
+*   return ImageViewHolder(imageView)
+*   }
+*
+*   onBindViewHolder(holder: ImageViewHolder, position: Ine){
+*
+*   }
+* getItemCount
+* }
+*
+* class Stack<T>(t: T){
+*   val someValue = t
+*
+*   fun pop() : T{
+*       return someValue
+*   }
+*   fun push(t:T){
+*       someValue = t
+*   }
+*
+*}
+*
+*
+*
+* myStack = Stack<String>("hello")
+* myStack.push
+* */
